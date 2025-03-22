7b367adf120963ad74264453d241fbaffc6aca7c
MahaAhmed11 <443200734@student.ksu.edu.sa>
2025-03-22 22:56:43 +0300

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -2,6 +2,6 @@
 session_start();     // بدء الجلسة
 session_unset();     // إزالة جميع متغيرات الجلسة
 session_destroy();   // إنهاء الجلسة
-header("Location: index.php"); // إعادة التوجيه للصفحة الرئيسية أو صفحة تسجيل الدخول
+header("Location: index.html"); // إعادة التوجيه للصفحة الرئيسية أو صفحة تسجيل الدخول
 exit();
 ?>
