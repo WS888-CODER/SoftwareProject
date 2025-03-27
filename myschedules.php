55e18501d5d33284089279b31699c5a06e0bb35c
v <halooh2004@gmail.com>
2025-03-27 03:40:27 +0300

    Update myschedules.php

--- myschedules.php
+++ myschedules.php
@@ -195,6 +195,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FARRAH | My Schedules </title>
+  <link rel="icon" type="image/png" href="images/logo.png">
   <link rel="stylesheet" href="css/MySchedules.css">
 </head>
 
