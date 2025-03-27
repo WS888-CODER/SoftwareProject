2fe80fe2d078574c696d854d4e0e9715e8f13c73
v <halooh2004@gmail.com>
2025-03-27 03:39:19 +0300

    Update DynamicDestination.php

--- DynamicDestination.php
+++ DynamicDestination.php
@@ -105,6 +105,7 @@ $averageRating = $totalReviews ? array_sum(array_column($reviews, 'Rating')) / $
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo htmlspecialchars($destination['Name']); ?></title>
+  <link rel="icon" type="image/png" href="images/logo.png">
   <link rel="stylesheet" href="css/DestnitionStyle.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
