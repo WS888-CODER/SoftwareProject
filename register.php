c0f740c29326e1bb00a9a9979b778ecbf24a377a
Ramaalomair <162565233+Ramaalomair@users.noreply.github.com>
2025-04-01 03:31:24 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include 'db.php';
 
