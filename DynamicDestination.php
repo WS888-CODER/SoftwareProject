a610caed35bbfe3351b95f0ecf3bdd4c33278917
v <halooh2004@gmail.com>
2025-07-08 10:13:17 +0300

    Update DynamicDestination.php

--- DynamicDestination.php
+++ DynamicDestination.php
@@ -109,6 +109,7 @@ $averageRating = $totalReviews ? array_sum(array_column($reviews, 'Rating')) / $
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo htmlspecialchars($destination['Name']); ?></title>
   <link rel="icon" type="image/png" href="images/logo.png">
+  <link rel="stylesheet" href="css/baiscStyle.css">
   <link rel="stylesheet" href="css/DestnitionStyle.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <link rel="stylesheet" href="https://unpkg.
